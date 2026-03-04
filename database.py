63c0e1faa3f84cebf16384c341b5fb7e3c749123
peedepaula <peedepaula13@gmail.com>
2026-03-04 16:08:12 -0300

    Mudando de banco de dados de MySql para o postgres

--- database.py
+++ database.py
@@ -1,18 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from sqlalchemy.engine import URL
+from config import settings
 
-url = URL.create(
-    drivername="mysql+pymysql",
-    username="root",
-    password="Opt@2020",
-    host="localhost",
-    port=3306,
-    database="doc_code"
-)
-
-engine = create_engine(url)
-print("URL REAL USADA:", engine.url)
+engine = create_engine(settings.DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
